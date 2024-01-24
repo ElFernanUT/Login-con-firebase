9721ab6747d37c7f723fcde830ec303fd2fd8c82
Alfredcss <alfre@Ferrusca.com>
2024-01-23 21:54:24 -0600

    actualización de login,registrar y cerrar sesión

--- js/firebase.js
+++ js/firebase.js
@@ -1,7 +1,8 @@
-        // Your web app's Firebase configuration
-        // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
 
-        const firebaseConfig = {
+// Configuración de tu proyecto Firebase
+const firebaseConfig = {
             apiKey: "AIzaSyBJv-QyeXXPW1eak3_J1H0gKvD-Fws1NGU",
             authDomain: "login-9e1c9.firebaseapp.com",
             projectId: "login-9e1c9",
@@ -9,10 +10,7 @@
             messagingSenderId: "567143214740",
             appId: "1:567143214740:web:ef042462a8a2ad5cb24e81",
             measurementId: "G-MEYNYZ4934"
-          };
-        
-          // Initialize Firebase
-          export const app = initializeApp(firebaseConfig);
-  
-  
-          
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
